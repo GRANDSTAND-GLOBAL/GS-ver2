abab016de1738666f5d97a4b7575577521cd6d3c
Marc-DT <92690960+Marc-DT@users.noreply.github.com>
2022-05-24 11:51:51 -0700

    Added 0524

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/',(req,res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-const queryString = window.location.search;
+const queryString = window.location.href;
 console.log(queryString);
 const urlParms = new URLSearchParams(queryString);
 const eventID = urlParms.get('eventID');

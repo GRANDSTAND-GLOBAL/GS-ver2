3b8119789654e6609324bddb263220bf9c04b670
GRANDSTAND Global, Inc <92690960+GRANDSTAND-GLOBAL@users.noreply.github.com>
2022-06-10 12:09:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,137 +1,76 @@
   'use strict';
-    const AccessToken = require('twilio').jwt.AccessToken;
-    const eventID = "EVENT_ID_Room_Name";
-    const VideoGrant = AccessToken.VideoGrant;
-
     const { createLocalTracks, createLocalVideoTrack, connect } = require('twilio-video');
-
-    const twilioAccountSid = "ACc69d00af99171ab03e90e5011747f9d5";
-    const twilioApiKey = "SK9d565e42549d33f38c6636d237db14c6";
-    const twilioApiSecret = "XU7Dk6ebqE6JIXGjXXUfce3DhxCohbW9";
     
+    const { createToken, tokenID } = require('./getAuth.js');
+
     const pageHeader = document.getElementById("page-header");
     const joinButton = document.getElementById("btn-start");
     const leaveButton = document.getElementById("btn-end");
+     const clearButton = document.getElementById("btn-att-end");
     const modalCtrl = document.querySelector(".ctrl")
-    const headerVideo = document.getElementById("hdr");
+    const headerVideo = document.getElementById("hdr-host");
+    const headerVideoA = document.getElementById("hdr-attendee");
+    const displayIcon = document.getElementById("att-icon");
     const modalVideo = document.querySelector(".modal");
     const modalAtt = document.querySelector(".att");
 
     const Twilio = require('twilio-video');
+
     const identity = "marc@gmail.com";
-    let tokenReturn;
+    const eventID = "EVENT_ID_Room_Name";
+    const displayName = "Bob";
 
-    let localStream = null;
-    
-    // Create Video Grant and Access Token
-    const videoGrant = new VideoGrant({
-           room: eventID,
-    });
-  
-    const token = new AccessToken (
-        twilioAccountSid,
-        twilioApiKey,
-        twilioApiSecret,
-        {identity: identity}
-    );
-  
-    token.addGrant(videoGrant);
-    tokenReturn = token.toJwt();
+    let token = "";
 
-    headerVideo.innerText = "Preview";
+    token = createToken( eventID, identity, displayName );
+    //userIDs.push(userID)
+
+    headerVideo.innerText = "Attendee";
     pageHeader.innerText = "Host";
 
+    console.log ("++display name:", displayName);
+    console.log ("++eventID:", eventID);
+    console.log ("++identity:", identity);
+    console.log ("++token:", token);
+
     // preview screen
-        createLocalVideoTrack({
-          audio : true,
-          video : 640,
-          width: 160,
-          height: 120
-        }).then(videoTrack => {
+      createLocalVideoTrack({
+        audio : true,
+        video : 640,
+        width: 160,
+        height: 120
+      }).then(videoTrack => {
           modalVideo.appendChild(videoTrack.attach());
-        });  
+      });  
 
-    joinButton.disabled = false;
-      console.log("before join")
-
+      joinButton.disabled = false;
+      clearButton.disabled = false;
 
       joinButton.onclick = () => {
-      console.log("join clicked");  
-      connect(tokenReturn, { 
+      
+      connect(token, { 
             audio: true,
             name: eventID,
             video: { width: 640 }
         }).then(room => {
-                  console.log(`Successfully joined a Room: ${room.name}`);
+                console.log(`Successfully joined a Room: ${room.name}`);
                 const localParticipant = room.localParticipant;
-                  console.log(`Connected to the Room as LocalParticipant "${localParticipant.identity}"`);
-//                videoRoom = room;
-                  console.log("participant connected");
+                console.log(`Connected to the Room as LocalParticipant "${localParticipant.identity}"`);
+//              videoRoom = room;
+                console.log("participant connected");
                 joinButton.disabled = true;
-                leaveButton.disabled = false;
-//                videoTrack = { width : 640, height: 480 };
-//console.log("before app.get");
-//app.get('/', (req, res) => {
-//    res.send(`<h1>hello world</h1>`);
-//  res.send(`<h1>${req.params.eventID}</h1>`);
-//});
-//console.log("after app.get");
-
-});
+            });
     }
     leaveButton.onclick = () => {
         modalVideo.style.display = "none";
           console.log('leave room');
           modalCtrl.style.display = "none";
           modalAtt.style.display = "block";
-          pageHeader.innerText = "Attendee";        
-//        leaveRoomButton.disabled = true;
-  //      joinButtonProp.style.displayed = "none";
-    //    leaveButtonProp.style.displayed = "none";
+          headerVideoA.innerText = displayName;
+          pageHeader.innerText = "Attendee";
+          leaveButton.disabled = true;        
     }
-    //          room.on("participantDisconnected", participantDisconnected);
-//                room.once("disconnected", (error) => {
- //                   room.participants.forEach(participantDisconnected)}
-    //        });
-  //              joinRoomButton.disabled = true;
-    //            leaveRoomButton.disabled = false;
-      //          leaveRoomButton.onclick = () => {
-        //            videoRoom.disconnect();
-          //          console.log(`Disconnected from Room ${videoRoom.name}`);
-            //        joinRoomButton.disabled = false;
-              //      leaveRoomButton.disabled = true;
- //               }; 
-      //     });
 
-//        console.log("after join");
-//
-//const participantConnected = (participant) => {
-//        console.log(`Participant ${participant.identity} connected'`);
-//
-//        const div = document.createElement('div');
-  //      div.id = participant.sid;
-    //
-      //  console.log('here in connected');
-//
-  //      participant.on('trackSubscribed', track => trackSubscribed(div, track));
-    //    participant.on('trackUnsubscribed', trackUnsubscribed);
-  //
-    //    participant.tracks.forEach(publication => {
-      //      if (publication.isSubscribed) {
-        //        trackSubscribed(div, publication.track);
-    //        };
-      //  });
-//    document.body.appendChild(div);
-//};
-//
-//const participantDisconnected = (participant) => {
-//        console.log(`Participant ${participant.identity} disconnected.`);
-//        document.getElementById(participant.sid).remove();
-//};
-//
-//const trackSubscribed = (div, track) => {
-//        div.appendChild(track.attach());
-//};
-//
-//const trackUnsubscribed = (track) => {
-//        track.detach().forEach(element => element.remove());
+    clearButton.onclick = () => {
+        displayIcon.style.display = "block";
+        }

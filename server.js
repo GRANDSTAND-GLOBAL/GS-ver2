eeb096cac20de901331b54c0e83ebdf02bc7975a
GRANDSTAND Global, Inc <92690960+GRANDSTAND-GLOBAL@users.noreply.github.com>
2022-06-10 12:08:28 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
 require('dotenv').load();
 const express = require('express');
-const http = require('http');
-const { Http2ServerResponse } = require('http2');
-const https = require('https');
-const path = require('path');
-const { nextTick } = require('process');
 const app = express();
+const { createToken, displayName, eventID, identity } = require('./getAuth.js');
 
 app.use(function(res, req, next) {
   next();
 });
 
-app.get('/', (req, res, next) => {
-  const eventID = req.query.eventID;
-  const displayName = req.query.displayName
-  console.log("here is:", eventID, "and user:", displayName);
-  next();
-});
+  app.get('/', (req, res, next) => {
+    let displayName = req.query.displayname;
+    let eventID = req.query.eventID;
+    let identity = req.query.identity;
+    next();
+  });    
 
-app.use( express.static( __dirname + '/' ) );
+  app.use( express.static( __dirname + '/' ) );
 
 const port = parseInt(process.env.PORT) || 3000;
 app.listen(port, () => {
   console.log(`Grandstand: listening on port ${port}`);
 });
+module.exports = { displayName, eventID, identity };

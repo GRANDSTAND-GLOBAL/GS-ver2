586124cacc5ec2b9a1510c0de0fc1f18d7756d6f
GRANDSTAND Global, Inc <92690960+GRANDSTAND-GLOBAL@users.noreply.github.com>
2022-06-23 14:35:27 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 require('dotenv').load();
 const express = require('express');
+var fs = require("fs");
 const app = express();
-const { createToken, displayName, eventID, identity } = require('./getAuth.js');
+const { createToken, saveToken, displayName, eventID, identity } = require('./getAuth.js');
 
-app.use(function(res, req, next) {
-  next();
-});
+let userDef = "{ ";
+
+  app.use(function(res, req, next) {
+    next();
+  });
 
   app.get('/', (req, res, next) => {
     let displayName = req.query.displayname;
     let eventID = req.query.eventID;
     let identity = req.query.identity;
-    next();
-  });    
+      userDef += '"eventID" : ' + JSON.stringify(eventID) + ", ";
+      userDef += '"identity" : ' + JSON.stringify(identity) + ", ";
+      userDef += '"displayName" : ' + JSON.stringify(displayName) + " }";      
+    fs.writeFileSync('./userFile.json', userDef);
+      next();
+  });   
 
   app.use( express.static( __dirname + '/' ) );
 

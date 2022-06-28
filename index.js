6351b0d73a0596ba153ccfe3e6422253fb09c450
GRANDSTAND Global, Inc <92690960+GRANDSTAND-GLOBAL@users.noreply.github.com>
2022-06-28 08:43:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-  'use strict';
+'use strict';
     const { createLocalTracks, createLocalVideoTrack, connect } = require('twilio-video');
     
     const { createToken } = require('./getAuth.js');
@@ -9,81 +9,97 @@
     const Twilio = require('twilio-video');
     
     const pageHeader = document.getElementById("page-header");
+    const getStartedButton = document.getElementById("btn-start-sync");
     const joinButton = document.getElementById("btn-start");
     const leaveButton = document.getElementById("btn-end");
     const clearButton = document.getElementById("btn-att-end");
-    const modalCtrl = document.querySelector(".ctrl")
-    const headerVideo = document.getElementById("hdr-host");
-    const headerVideoA = document.getElementById("hdr-attendee");
-    const displayIcon = document.getElementById("att-icon");
-    const modalVideo = document.querySelector(".modal");
-    const modalAtt = document.querySelector(".att");
+    const btnCtrl = document.querySelector(".ctrl");
+    const subHeader = document.getElementById("sub-header");
+    const displayIcon = document.getElementById("display-icon");
+    const hostVideo = document.querySelector(".host");
 
     let eventID = "";
     let identity = "";
     let displayName = "";
-  
+    let userType = '';
+    getStartedButton.style.display = "block";  
+
     getUser();
-    joinButton.onclick = () => {
-      startRoom();
+
+    getStartedButton.onclick = () => {
+      getStartedButton.style.display = "none";
+      joinButton.style.display = "block"; 
+      leaveButton.style.display = "block";
+      clearButton.style.display = "none";
+      console.log ("userType", userType);
+      if ( userType == "Host" ) {
+       startRoom();
+      } else {
+        joinRoom();
+      }
     }
 
-    function getUser() {
-      fetch('./userFile.json')
-      .then(response => response.json())
-      .then(jsonResponse => {
-          eventID = jsonResponse.eventID;
-          identity = jsonResponse.identity;
-          displayName = jsonResponse.displayName;
-        });
-
-      }
+function getUser() {
+  fetch('./userFile.json')
+    .then(response => response.json())
+    .then(jsonResponse => {
+        eventID = jsonResponse.eventID;
+        identity = jsonResponse.identity;
+        displayName = jsonResponse.displayName;
+        userType = jsonResponse.userType;
+    });
+  }
+  function joinRoom() {    
+    pageHeader.innerText = "Attendee";
+    subHeader.innerText = displayName;
+  }
 
 function startRoom() {
-    let token = "";
+  let token = "";
+  token = createToken( eventID, identity );
+  
+  pageHeader.innerText = "Host";
+  subHeader.innerText = eventID;
 
-    token = createToken( eventID, identity );
-    
-    headerVideo.innerText = eventID;
-    pageHeader.innerText = "Host";
+// preview screen
+  createLocalVideoTrack({
+    audio : true,
+    video : 640,
+    width: 160,
+    height: 120
+  }).then(videoTrack => {
+      hostVideo.appendChild(videoTrack.attach());
+    });  
 
-    // preview screen
-      createLocalVideoTrack({
-        audio : true,
-        video : 640,
-        width: 160,
-        height: 120
-      }).then(videoTrack => {
-          modalVideo.appendChild(videoTrack.attach());
-      });  
+  joinButton.disabled = false;
+  clearButton.disabled = false;
 
-      joinButton.disabled = false;
-      clearButton.disabled = false;
-
-      joinButton.onclick = () => {
-      
+  joinButton.onclick = () => {  
       connect( token, { 
-            audio: true,
-            name: eventID,
-            video: { width: 640 }
-        }).then(room => {
-                console.log(`Successfully joined a Room: ${room.name}`);
-                const localParticipant = room.localParticipant;
-                console.log(`Connected to the Room as LocalParticipant "${localParticipant.identity}"`);
-                joinButton.disabled = true;
-            });
-    }
-    leaveButton.onclick = () => {
-        modalVideo.style.display = "none";
-          console.log('leave room');
-          modalCtrl.style.display = "none";
-          modalAtt.style.display = "block";
-          headerVideoA.innerText = displayName;
-          pageHeader.innerText = "Attendee";
-          leaveButton.disabled = true;        
+        audio: true,
+        name: eventID,
+        video: { width: 640 }
+      }).then(room => {
+          console.log(`Successfully joined a Room: ${room.name}`);
+          const localParticipant = room.localParticipant;
+          console.log(`Connected to the Room as LocalParticipant "${localParticipant.identity}"`);
+          joinButton.disabled = true;
+        });
     }
 
-    clearButton.onclick = () => {
-        displayIcon.style.display = "block";
-        }
-      }
+  leaveButton.onclick = () => {
+    hostVideo.style.display = "none";
+    console.log('leave room');
+    pageHeader.innerText = "Attendee";
+    subHeader.innerText = displayName;
+    joinButton.style.display = "none";
+    leaveButton.style.display = "none";
+    clearButton.style.display = "block";       
+  }
+
+  clearButton.onclick = () => {
+    displayIcon.style.display = "block";
+    clearButton.style.display = "none";  
+    btnCtrl.style.display = "none";
+  }          
+}

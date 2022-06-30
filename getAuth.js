ef61e7d39925957ae386fb7f40e85a6252c1fbf5
GRANDSTAND Global, Inc <92690960+GRANDSTAND-GLOBAL@users.noreply.github.com>
2022-06-29 21:23:54 -0700

    Update getAuth.js

--- getAuth.js
+++ getAuth.js
@@ -1,32 +1,29 @@
-'use strict';
-  require('dotenv').load();
-  //const fs = require("fs");
+  'use strict';
   const userFile = require('./userFile.json');
-
   const { createLocalTracks, createLocalVideoTrack, connect } = require('twilio-video');
   const AccessToken = require('twilio').jwt.AccessToken;
   const VideoGrant = AccessToken.VideoGrant;
 
-  const twilioAccountSid = "ACc69d00af99171ab03e90e5011747f9d5";
-  const twilioApiKey = "SK9d565e42549d33f38c6636d237db14c6";
-  const twilioApiSecret = "XU7Dk6ebqE6JIXGjXXUfce3DhxCohbW9";
+  function createToken( eventID, identity, account_Sid, api_Key, api_Secret ) {
 
-  var eventID;
-  var identity;
-  
-  let tokenID = "";
-
-  function createToken( eventID, identity ) {
+    var eventID;
+    var identity;
+    var account_Sid;
+    var api_Key;
+    var api_Secret;
+    let tokenID = "";
+    
     const videoGrant = new VideoGrant({
       room: eventID
     });
 
     const token = new AccessToken (
-        twilioAccountSid,
-        twilioApiKey,
-        twilioApiSecret,
+        account_Sid,
+        api_Key,
+        api_Secret,
       {identity: identity}
     );
+
     token.addGrant(videoGrant);
     tokenID = token.toJwt();
     return tokenID;
